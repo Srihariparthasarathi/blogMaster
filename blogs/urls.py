d5026e0723d0b7794079f11a8b3cb3f195fd5390
Srihariparthasarathi <srihariparthasarathi20@gmail.com>
2024-08-12 15:18:13 +0530

    posts- class based function created. git, post request is handled , valditation form is created and set for Posts

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/',views.home, name="home"),
- 
+    path('posts',views.Posts.as_view(), name='posts-list'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.http import JsonResponse, HttpResponseNotFound
+from django.views import View
+from .models import *
+from .forms import *
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
+
+@method_decorator(csrf_exempt, name='dispatch')
+class Posts(View):
+
+    def get(self, request, *args, **kwargs):
+
+        posts = Post.objects.all()
+
+        if not posts.exists():
+            return JsonResponse({"message": "no blog is avalilable."}, status = 404)
+        
+        return JsonResponse(list(posts.values()), safe=False)
+    
+    def post(self, request, *args, **kwargs):
+
+        try:
+             data = json.loads(request.body)
+        except:
+            return JsonResponse({"error": "Invalid JSON data"}, status=400)
+
+        form = PostsForm(data=data)
+
+        if form.is_valid():
+            post = form.save()
+            response_data = {
+                'id': post.id,
+                'title': post.title,
+                'content': post.content,
+                'created_at': post.created_at,
+            }
+            return JsonResponse(response_data, status=201)  
+        
+        else:
+            return JsonResponse(form.errors, status=400)
